dc5fce6c03d64380b3cb5785469409a8be7fcb40
piper4123w <kyle.lawson@marquette.edu>
2017-03-30 18:38:20 -0500

    its sooo close i can taste it

--- LimoAlgorithmsJava/src/Node.java
+++ LimoAlgorithmsJava/src/Node.java
@@ -8,12 +8,23 @@ public class Node {
 	double y;
 	double weight;
 	public boolean isCaller = false;
+	public int id;
 
 	public Node(double x2, double y2, Node p, double w) {
 		this.x = x2;
 		this.y = y2;
 		parent = p;
 		weight = w;
+		childList = new ArrayList<Node>();
+	}
+
+	public Node(Node c) {
+		x = c.x;
+		y = c.y;
+		id = c.id;
+		weight = c.weight;
+		isCaller = c.isCaller;
+		parent = c.parent;
 		childList = new ArrayList<Node>();
 	}
 
--- LimoAlgorithmsJava/src/dynamicLimo.java
+++ LimoAlgorithmsJava/src/dynamicLimo.java
@@ -7,6 +7,8 @@ public class dynamicLimo extends Limo {
 	Node nextDest;
 	Node root;
 
+	int nodeID = 1;
+
 	public dynamicLimo(double x, double y) {
 		this.x = x;
 		this.y = y;
@@ -21,9 +23,13 @@ public class dynamicLimo extends Limo {
 			if (x == targetX && y == targetY) {
 				if (nextDest.isCaller) {
 					passengerList.add(nextDest.caller);
+					callerList.remove(nextDest.caller);
+					solvePath();
 				} else {
 					passengerList.remove(nextDest.caller);
+					nextDest.parent = null;
 				}
+
 				nextDest = getNextMove();
 			}
 		}
@@ -32,42 +38,63 @@ public class dynamicLimo extends Limo {
 
 	public void addCaller(Caller c) {
 		callerList.add(c);
+		fastestPath = null;
 		solvePath();
 		nextDest = getNextMove();
 	}
 
 	private Node getNextMove() {
 		Node n = fastestPath;
+		if (n == null) {
+			System.out.println("no moves");
+		} else if (n.parent == null) {
+			System.out.println("Only move");
+			return n;
+		}
 		while (n.parent.parent != null) {
+			System.out.println(n.toString() + "->");
 			n = n.parent;
 		}
+		System.out.println();
 		return n;
 	}
 
 	private void solvePath() {
 		root = new Node(x, y, null, 0);
 		root.isCaller = false;
+		nodeID = 1;
+		System.out.println("there are " + callerList.size() + " callers & " + passengerList.size() + " passengers");
 		for (Caller c : callerList) {
 			Node n = new Node(c.x, c.y, root, c.weight);
 			n.caller = c;
 			n.isCaller = true;
+			n.id = nodeID++;
 			root.childList.add(n);
 		}
 		for (Caller c : passengerList) {
 			Node n = new Node(c.x, c.y, root, c.weight);
+			n.id = nodeID++;
 			root.childList.add(n);
 		}
+		System.out.println("There are " + (nodeID - 1) + " possible destinations");
 		getPath(root);
 		System.out.println("Got dat path boiiiii");
 	}
 
 	public void getPath(Node node) {
+		// System.out.println("node :" + node.toString() + " id=" + node.id);
 		if (!node.childList.isEmpty()) {
 			// node.sortChildren();
 			for (Node n : node.childList) {
+				// System.out.println("Child in questions = " + n.id + " addr: "
+				// + n.toString());
 				for (Node c : node.childList) {
-					if (!c.equals(n)) {
-						n.childList.add(c);
+					if (c.id != n.id) {
+						Node newChild = new Node(c);
+						c.parent = n;
+						n.childList.add(newChild);
+						// System.out.println("added " + c.id + " as " +
+						// newChild.toString());
 					}
 				}
 
@@ -82,6 +109,7 @@ public class dynamicLimo extends Limo {
 				getPath(n);
 			}
 		} else {
+			// System.out.println();
 			if (fastestPath == null || fastestPath.weight > node.weight)
 				fastestPath = node;
 		}
